f8653ff3143ecddcebc904c7a81b87260e4c7eb5
Natalia <natalia@Natalias-MacBook-Pro-4.local>
2023-08-27 19:40:08 -0500

    co2

--- Individual Workspaces/Natalia Lopez/Map/co2_script.js
+++ Individual Workspaces/Natalia Lopez/Map/co2_script.js
@@ -1,86 +1,73 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Load the CO2 data and populate the year dropdown
-    fetch('co2_data.csv')
-        .then(response => response.text())
-        .then(data => {
-            const rows = data.split('\n');
-            const yearSelect = document.getElementById('yearSelect');
+// Load CSV file and create bar chart
+d3.csv("co2_data.csv").then(function(data) {
+  const selYearDropdown = document.getElementById("selYear");
 
-            // Extract unique years from the data
-            const years = new Set();
-            for (let i = 1; i < rows.length; i++) {
-                const cols = rows[i].split(',');
-                if (cols.length >= 3) { // Ensure proper column index
-                    years.add(cols[0]);
-                }
+  // Extract years from the data
+  const years = [...new Set(data.map(d => +d.year))];
+
+  // Populate the "Select Year" dropdown
+  years.forEach(year => {
+    const option = document.createElement("option");
+    option.value = year;
+    option.textContent = year;
+    selYearDropdown.appendChild(option);
+  });
+
+  // Event listener for the "Select Year" dropdown
+  selYearDropdown.addEventListener("change", function() {
+    const selectedYear = +this.value;
+
+    // Filter data for the selected year
+    const filteredData = data.filter(d => +d.year === selectedYear);
+
+    // Process data to calculate average trend by month
+    const monthlyAvg = d3.rollups(
+      filteredData,
+      v => d3.mean(v, d => +d.trend),
+      d => +d.month
+    );
+
+    const months = monthlyAvg.map(d => d[0]);
+    const avgTrends = monthlyAvg.map(d => d[1]);
+
+    // Create Chart.js bar chart
+    const ctx = document.getElementById("barChart").getContext("2d");
+    const barChart = new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: months,
+        datasets: [{
+          label: `Average Trend for ${selectedYear}`,
+          data: avgTrends,
+          backgroundColor: "rgba(75, 192, 192, 0.2)",
+          borderColor: "rgba(75, 192, 192, 1)",
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+            title: {
+              display: true,
+              text: "Average Trend"
             }
-
-            // Populate the year dropdown
-            years.forEach(year => {
-                const option = document.createElement('option');
-                option.value = year;
-                option.textContent = year;
-                yearSelect.appendChild(option);
-            });
-
-            // Initialize chart with default year
-            updateChart();
-        });
+          },
+          x: {
+            title: {
+              display: true,
+              text: "Month"
+            }
+          }
+        },
+        plugins: {
+          title: {
+            display: true,
+            text: `Average Trend by Month for ${selectedYear}`
+          }
+        }
+      }
+    });
+  });
 });
 
-function updateChart() {
-    const selectedYear = document.getElementById('yearSelect').value;
-    
-    // Load the CO2 data
-    fetch('co2_data.csv')
-        .then(response => response.text())
-        .then(data => {
-            const rows = data.split('\n');
-            const months = new Map();
-
-            for (let i = 1; i < rows.length; i++) {
-                const cols = rows[i].split(',');
-                if (cols.length >= 5 && cols[0] === selectedYear) {
-                    const month = cols[1];
-                    const trend = parseFloat(cols[4]);
-
-                    if (!months.has(month)) {
-                        months.set(month, []);
-                    }
-
-                    months.get(month).push(trend);
-                }
-            }
-
-            // Calculate average trend for each month
-            const x = Array.from(months.keys());
-            const y = x.map(month => {
-                const trendValues = months.get(month);
-                const averageTrend = trendValues.reduce((sum, value) => sum + value, 0) / trendValues.length;
-                return averageTrend;
-            });
-
-            // Create the bar chart
-            const chartDiv = document.getElementById('chart');
-            const data = [{
-                x: x,
-                y: y,
-                type: 'bar',
-                marker: {
-                    color: 'blue'
-                }
-            }];
-
-            const layout = {
-                title: `Average CO2 Trend in ${selectedYear}`,
-                xaxis: {
-                    title: 'Month',
-                },
-                yaxis: {
-                    title: 'Average CO2 Trend',
-                },
-            };
-
-            Plotly.newPlot(chartDiv, data, layout);
-        });
-}
