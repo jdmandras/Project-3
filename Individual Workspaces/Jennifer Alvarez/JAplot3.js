7f4bee51ce9a2cfd7761295c014b3ab91936434b
Jennifer Alvarez <alvarezjennifer472@gmail.com>
2023-08-27 22:34:16 -0500

    final plot #3 historical sea level w/ major events

--- Individual Workspaces/Jennifer Alvarez/JAplot3.js
+++ Individual Workspaces/Jennifer Alvarez/JAplot3.js
@@ -29,7 +29,119 @@ async function fetchCSVAndCreateGraph() {
         },
         yaxis: {
             title: 'Sea Level Rise (cm)'
-        }
+        },
+        annotations: [
+            {
+                x: 1883,
+                y: 0.5, // Adjust the y-coordinate of the callout as needed
+                xref: 'x',
+                y: seaLevelData[years.indexOf('1883')], 
+                text: "Krakatoa Volcano Eruption",
+                showarrow: true,
+                arrowhead: 2,
+                ax: 50,
+                ay: -40
+            },
+            // {
+            //     x: 1903,
+            //     y: 0.5, // Adjust the y-coordinate of the callout as needed
+            //     xref: 'x',
+            //     y: seaLevelData[years.indexOf('1903')], 
+            //     text: "Ford Motor Company Formed",
+            //     showarrow: true,
+            //     arrowhead: 2,
+            //     ax: 0,
+            //     ay: -40
+            // },
+            {
+                x: 1914,
+                y: 0.5, // Adjust the y-coordinate of the callout as needed
+                xref: 'x',
+                y: seaLevelData[years.indexOf('1914')], 
+                text: "World War I begins",
+                showarrow: true,
+                arrowhead: 2,
+                ax: 0,
+                ay: -40
+            },
+            {
+                x: 1918,
+                y: 0.5, // Adjust the y-coordinate of the callout as needed
+                xref: 'x',
+                y: seaLevelData[years.indexOf('1918')], 
+                text: "Spanish Flu Pandemic begins",
+                showarrow: true,
+                arrowhead: 2,
+                ax: 0,
+                ay: 40
+            },
+            {
+                x: 1929,
+                y: seaLevelData[years.indexOf('1929')], // Adjust the y-coordinate of the callout as needed
+                xref: 'x',
+                yref: 'y',
+                text: "The Great Depression begins",
+                showarrow: true,
+                arrowhead: 2,
+                ax: 0,
+                ay: -40
+            },
+            {
+                x: 1939,
+                y: seaLevelData[years.indexOf('1939')], // Adjust the y-coordinate of the callout as needed
+                xref: 'x',
+                yref: 'y',
+                text: "World War II begins",
+                showarrow: true,
+                arrowhead: 2,
+                ax: 0,
+                ay: -40
+            },
+            {
+                x: 1945,
+                y: seaLevelData[years.indexOf('1945')], // Adjust the y-coordinate of the callout as needed
+                xref: 'x',
+                yref: 'y',
+                text: "Atomic bombings of Hiroshima and Nagasaki",
+                showarrow: true,
+                arrowhead: 2,
+                ax: 0,
+                ay: 40
+            },
+            {
+                x: 1955,
+                y: seaLevelData[years.indexOf('1955')], // Adjust the y-coordinate of the callout as needed
+                xref: 'x',
+                yref: 'y',
+                text: "Vietnam War begins",
+                showarrow: true,
+                arrowhead: 2,
+                ax: 0,
+                ay: -40
+            },
+            {
+                x: 1986,
+                y: seaLevelData[years.indexOf('1986')], // Adjust the y-coordinate of the callout as needed
+                xref: 'x',
+                yref: 'y',
+                text: "Chernobyl Nuclean Disaster",
+                showarrow: true,
+                arrowhead: 2,
+                ax: 0,
+                ay: -40
+            },
+            {
+                x: 2003,
+                y: seaLevelData[years.indexOf('2003')], // Adjust the y-coordinate of the callout as needed
+                xref: 'x',
+                yref: 'y',
+                text: "9/11 New York Terrorist Attacks",
+                showarrow: true,
+                arrowhead: 2,
+                ax: 0,
+                ay: -40
+            },
+        ]
     };
 
     Plotly.newPlot('historicalplot', plotData, layout);
