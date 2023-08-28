1524325bbcae8b7db3b46744b763514c91d094c2
mavingill <129801026+mavingill@users.noreply.github.com>
2023-08-28 18:20:59 -0500

    Add files via upload

--- DashboardFinal/script_MG.js
+++ DashboardFinal/script_MG.js
@@ -47,7 +47,7 @@ function selectionMade(selectedSample, selectedDataset) {
     // Extract relevant columns
     let anomalies = sampleData.map(row => parseFloat(row.Anomaly));
     let years = sampleData.map(row => parseInt(row.Year));
-    let months = sampleData.map(row => parseInt(row.Year.slice(-2)) - 1);
+    let months = sampleData.map(row => parseInt(row.Year.slice(-2)));
     
     // Extract month names or numbers
     let monthLabels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -90,7 +90,7 @@ function selectionMade(selectedSample, selectedDataset) {
     Plotly.newPlot("bar", selectedBar, selectedBarLayout);
 
     // Define your 'size' array with appropriate values
-    let sizeArray = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+    let sizeArray = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60];
 
     // Function to format tick labels
     function customTickFormat(year) {
