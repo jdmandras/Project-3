d930cdaa8920b1cbe679e02e1af80907271dd1f5
mavingill <129801026+mavingill@users.noreply.github.com>
2023-08-28 20:42:50 -0500

    Add files via upload

--- Individual Workspaces/Mavin Gill/Project_3_MGill.js
+++ Individual Workspaces/Mavin Gill/Project_3_MGill.js
@@ -8,6 +8,9 @@ function createOrUpdateDonutChart(chartCanvas, data, labels, colors) {
   }
 
   let ctx = document.getElementById(chartCanvas).getContext('2d');
+  ctx.canvas.width = 400; // Set the desired width
+  ctx.canvas.height = 400; // Set the desired height
+
   chartInstances[chartCanvas] = new Chart(ctx, {
     type: 'doughnut', // Use 'doughnut' type for a donut chart
     data: {
@@ -48,6 +51,7 @@ function selectionMade(selectedSample, selectedDataset) {
     let anomalies = sampleData.map(row => parseFloat(row.Anomaly));
     let years = sampleData.map(row => parseInt(row.Year));
     let months = sampleData.map(row => parseInt(row.Year.slice(4)));
+    console.log(months)
     // Extract month names or numbers
     let monthLabels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     
@@ -115,7 +119,7 @@ function selectionMade(selectedSample, selectedDataset) {
       },
       text: anomalies,
     }];
-  
+    
     
     // Bubble chart layout
     let selectedBubbleLayout = {
@@ -186,4 +190,57 @@ d3.select("#selDataset").on("change", function (e) {
   let selectedSample = e.target.value;
   let selectedDataset = d3.select("#datasetSelector").property("value");
   selectionMade(selectedSample, selectedDataset);
+});
+
+
+
+
+
+
+
+
+
+var map = L.map('map').setView([data[0].Latitude, data[0].Longitude], 1);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+data.forEach(function(item) {
+    var aqiCategory = item['AQI Category'];
+    var markerColor;
+
+    if (aqiCategory === 'Good') {
+        markerColor = 'green';
+    } else if (aqiCategory === 'Moderate') {
+        markerColor = 'yellow';
+    } else if (aqiCategory === 'Unhealthy for Sensitive Groups') {
+        markerColor = 'orange';
+    } else {
+        markerColor = 'red';
+    }
+
+    var marker = L.marker([item.Latitude, item.Longitude], {
+        icon: L.icon({
+            iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-' + markerColor + '.png',
+            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+        })
+    }).bindPopup(
+        "City: " + item.City +
+        "<br>Country: " + item.Country +
+        "<br>AQI: " + item['AQI Value'] +
+        "<br>AQI Category: " + item['AQI Category'] +
+        "<br>CO AQI Value: " + item['CO AQI Value'] +
+        "<br>AQI CO AQI Category: " + item['CO AQI Category'] +
+        "<br>OZone AQI Value: " + item['Ozone AQI Value'] +
+        "<br>Ozone AQI Category: " + item['Ozone AQI Category'] +
+        "<br>NO2 AQI Value: " + item['NO2 AQI Value'] +
+        "<br>NO2 AQI Category: " + item['NO2 AQI Category'] +
+        "<br>PM2.5 AQI Category: " + item['PM2.5 AQI Value'] +
+        "<br>PM2.5 AQI Category: " + item['PM2.5 AQI Category']
+    ).addTo(map);
 });
